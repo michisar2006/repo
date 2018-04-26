f66d8e12a07005048e6e90d7ac96f2f5d599216e
icnovaro <icnovaro@gmail.com>
2018-04-25 23:44:39 -0500

    adds the project and its settings

---
+++ pollap/pollap/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'pollap',
+    'HOST': 'localhost',
+    }
+}
