5153711513e5fd0f396e7c7327372177cd51405a
icnovaro <icnovaro@gmail.com>
2018-04-26 21:09:14 -0500

    removes pgdata 'cause windows

--- pollap/pollap/settings/base.py
+++ pollap/pollap/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- pollap/pollap/settings/local.py
+++ pollap/pollap/settings/local.py
@@ -5,11 +5,13 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'pollap',
-    'HOST': 'localhost',
+    'NAME': os.environ['POSTGRES_DB'],
+    'USER': os.environ['POSTGRES_USER'],
+    'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+    'HOST': os.environ['POSTGRES_HOST'],
+    'PORT': os.environ['POSTGRES_PORT'],
     }
 }
